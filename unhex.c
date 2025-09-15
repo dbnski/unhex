c3b25c9c672027a2d28bf6bd896088a2deb1e66d
Maciej Dobrzanski <git@dbnski.com>
2025-09-15 13:03:46 +0200

    Supports all raw binary literals

--- unhex.c
+++ unhex.c
@@ -4,29 +4,87 @@
 #include <unistd.h>
 
 #define BUF_SIZE 4096
-#define BIN_LEN  16   // our binary is 16 bytes
+#define BIN_SIZE 256
 
-enum State { TEXT, BINARY, ESCAPE };
+enum State { TEXT, QUOTED_STRING, BINARY, RAW };
 
-const char prefix[] = "_binary '";
+const unsigned char modifier[] = "_binary ";
 
-void print_binary(const unsigned char *bin, size_t blen)
+void print_binary(const unsigned char *buf, size_t blen)
 {
     printf("0x");
     for (size_t i = 0; i < blen; i++)
-        printf("%02x", bin[i]);
+    {
+        unsigned char c = buf[i];
+
+        switch (c)
+        {
+            case '\\':
+                switch (buf[i+1]) {
+                    case '\'':
+                    case '\"':
+                    case '\\':
+                    case '\0':
+                        continue;
+                    default:
+                        fprintf(stderr, "bad sequence: %d%d\n", buf[i], buf[i+1]);
+                        exit(1);
+                }
+        }
+        printf("%02x", buf[i]);
+    }
+}
+
+void print_string(const unsigned char *buf, size_t blen)
+{
+    putchar('\'');
+    fwrite(buf, 1, blen, stdout);
+    putchar('\'');
+}
+
+void print_raw(const unsigned char *buf, size_t blen)
+{
+    fwrite(buf, 1, blen, stdout);
+}
+
+void print(enum State mode, const unsigned char *buf, size_t blen)
+{
+    switch (mode)
+    {
+        case QUOTED_STRING:
+            print_string(buf, blen);
+            break;
+        case RAW:
+            print_raw(buf, blen);
+            break;
+        case BINARY:
+            print_binary(buf, blen);
+            break;
+        default:
+            fprintf(stderr, "unsupported mode: %d\n", mode);
+            exit(1);
+    }
 }
 
 int main(void) {
     enum State state = TEXT;
     unsigned char buf[BUF_SIZE];
-    unsigned char bin[BIN_LEN];
-    size_t blen = 0, n;
+    unsigned char bin[BIN_SIZE];
+    unsigned char *bptr = NULL;
+    unsigned char prev = 0;
+    size_t blen = 0, mlen = 0, n;
     int in_comment = 0, nl = 1;
-    int mlen = 0;
 
-    while ((n = fread(buf, 1, BUF_SIZE, stdin)) > 0) {
-        for (size_t i = 0; i < n; i++) {
+    if (BIN_SIZE >= BUF_SIZE)
+    {
+        fprintf(stderr, "BIN_SIZE must be smaller than BUF_SIZE!\n");
+        exit(1);
+    }
+
+    while ((n = fread(buf, 1, BUF_SIZE, stdin)) > 0)
+    {
+        for (size_t i = 0; i < n; i++)
+        {
             unsigned char c = buf[i];
 
             switch (state)
@@ -50,22 +108,27 @@ int main(void) {
                         continue;
                     }
 
-                    // try matching the prefix
-                    if (c == prefix[mlen]) {
+                    if (c == modifier[mlen])
+                    {
                         mlen++;
-                        // full prefix found
-                        if (mlen == sizeof(prefix) - 1) {
-                            state = BINARY;
-                            blen = 0;
+                        if (mlen == sizeof(modifier) - 1)
+                        {
                             mlen = 0;
                         }
                         continue;
                     }
-
-                    // write matching chars we've eaten to this point
-                    if (mlen > 0) {
-                        fwrite(prefix, 1, mlen, stdout);
+                    if (mlen > 0)
+                    {
+                        fwrite(modifier, 1, mlen, stdout);
                         mlen = 0;
+                    }
+
+                    if (c == '\'') // opening quote char
+                    {
+                        state = QUOTED_STRING;
+                        bptr = buf + i + 1;
+                        blen = 0;
+                        continue;
                     }
 
                     putchar(c);
@@ -73,60 +136,59 @@ int main(void) {
                         nl = 1;
                     }
                     break;
-                case ESCAPE:
-                    // test for known escaped characters
-                    switch (c)
+                case RAW:
+                    if (c == '\'' && prev != '\\')
                     {
-                        case '"':
-                        case '\'':
-                        case '\\':
-                        case '\0':
-                            if (blen == BIN_LEN)
-                            {
-                                fprintf(stderr, "binary value buffer overflow\n");
-                                exit(1);
-                            }
-                            bin[blen++] = c;
-                            break;
-                        default: // not one of the escaped characters
-                            if (blen + 1 == BIN_LEN)
-                            {
-                                fprintf(stderr, "binary value buffer overflow\n");
-                                exit(1);
-                            }
-                            bin[blen++] = '\\'; // recover non-escaping backslash
-                            bin[blen++] = c;
-                            break;
+                        state = TEXT;
                     }
-                    state = BINARY;
+                    putchar(c);
                     break;
-                case BINARY:
-                    switch (c)
+                case QUOTED_STRING:
+                    if (c < 0x20)
                     {
-                        case '\'': // ends binary string
-                            print_binary(bin, blen);
+                        state = BINARY;
+                    }
+                    /* fallthrough */
+                case BINARY:
+                    if (c == '\'') // closing quote candidate
+                    {
+                        if (prev != '\\') // quote char not escaped
+                        {
+                            print(state, bptr, blen);
+                            bptr = NULL;
+                            blen = 0;
                             state = TEXT;
-                            break;
-                        case '\\':
-                            state = ESCAPE;
-                            break;
-                        default:
-                            if (blen == BIN_LEN)
-                            {
-                                fprintf(stderr, "binary value buffer overflow\n");
-                                exit(1);
-                            }
-                            bin[blen++] = c;
-                            break;
+                        }
+                    }
+                    else
+                    {
+                        blen++;
+                        if (blen > BIN_SIZE) // too long
+                        {
+                            // dump as is, including the leading quote
+                            putchar('\'');
+                            print(RAW, bptr, blen - 1);
+                            putchar(c);
+
+                            bptr = NULL;
+                            blen = 0;
+                            state = RAW;
+                        }
                     }
                     break;
             }
-        }
-    }
 
-    if (state == BINARY && blen > 0) {
-        print_binary(bin, blen);
-        fprintf(stderr, "warning: incomplete binary sequence\n");
+            if (i == n - 1) // end of current buf
+            {
+                if (state == QUOTED_STRING || state == BINARY)
+                {
+                    memcpy(bin, bptr, blen);
+                    bptr = bin;
+                }
+            }
+
+            prev = c;
+        }
     }
 
     exit(0);
